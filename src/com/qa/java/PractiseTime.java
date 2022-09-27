83922dfab62a67cc13a66a5e6a075b6cbfaa3860
DanielleMassey <danimassey1998@gmail.com>
2022-09-27 14:45:32 +0100

    Conditional statements

---
+++ src/com/qa/java/PractiseTime.java
@@ -0,0 +1,46 @@
+package com.qa.java;
+import java.util.Scanner;
+
+public class PractiseTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		name = "Danielle";
+		
+		int age;
+		age = 23;
+		
+		boolean living;
+		living = true;
+		
+		Scanner userInp = new Scanner(System.in);
+		
+	while(living = true)
+	{
+		String response = userInp.nextLine();
+		
+		if(response.equals("birthday"))
+		{
+			age++;
+			System.out.println(name + " is " + age);
+		}
+		else if(response.equals("dead"))
+		{
+			living = false;
+			System.out.println(name + " has passed away.");
+		}
+		else
+		{
+			System.out.println("Something went wrong");
+		}
+	}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/qa/java/ifElseTest.java
@@ -0,0 +1,45 @@
+package com.qa.java;
+
+public class ifElseTest {
+
+	public static void main(String[] args) {
+
+		/*
+		int number;
+		number = 97;
+		if(number % 2 == 0)
+		{
+			System.out.println(number +" is even.");
+		}
+		else
+		{
+			System.out.println(number + " is odd.");
+		}
+		*/
+		
+		int experience;
+		experience = 7;
+		String department;
+		department = "S";
+		float salary;
+		salary = 25000;
+		
+		if(experience > 2 && department == "D")
+		{
+			salary = salary * 1.1f;
+			System.out.println("new salary is " + salary);
+		}
+		else if(experience > 5 && department == "S")
+		{
+			salary = salary * 1.08f;
+			System.out.println("new salary is " + salary);
+		}
+		else
+		{
+			System.out.println("Salary remains as " + salary);
+		}
+			
+
+	}
+
+}
